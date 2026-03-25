e77b8164504ae6c0bbe1bc16bdea624dae776863
Cassis733 <cassis733@gmail.com>
2026-03-24 22:45:55 -0300

    Aula 24-03

---
+++ src/atividades/atv-03/Index.js
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { View, Text, stylesSheet } from 'react-native';
+import styles from '../atv-02/styles';
+import styles from '../atv-02/styles';
+
+export default function Index() {
+    return(
+        <View style={styles.containes} >
+            <Text style={styles.paragraph} >
+                Exemplo 3
+            </Text>    
+        </View>
+    );
+
+}
+
+const styles = stylesSheet.creat({
+
+    
+})
